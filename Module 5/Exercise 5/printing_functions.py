1f240bdfe107dc8e40c2db870d574ac9b6172fab
tiagopf2 <156838810+tiagopf2@users.noreply.github.com>
2024-02-07 17:06:34 -0500

    Create printing_functions.py

---
+++ Module 5/Exercise 5/printing_functions.py
@@ -0,0 +1,11 @@
+def print_models(models):
+    """Simulate printing each model."""
+    print("Printing models:")
+    for model in models:
+        print(f"- {model}")
+
+def print_completed_models(completed_models):
+    """Print a message about each completed model."""
+    print("\nThe following models have been printed:")
+    for model in completed_models:
+        print(f"- {model}")
