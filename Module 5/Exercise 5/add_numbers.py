0e38cb66254f6ec3fab4776b7c878e1e541fbf88
tiagopf2 <156838810+tiagopf2@users.noreply.github.com>
2024-02-07 17:07:16 -0500

    Create add_numbers.py

---
+++ Module 5/Exercise 5/add_numbers.py
@@ -0,0 +1,24 @@
+import math_operations
+
+result1 = math_operations.add_numbers(5, 3)
+print("Result 1:", result1)
+
+from math_operations import add_numbers
+
+result2 = add_numbers(10, 7)
+print("Result 2:", result2)
+
+from math_operations import add_numbers as fn
+
+result3 = fn(8, 2)
+print("Result 3:", result3)
+
+import math_operations as mn
+
+result4 = mn.add_numbers(15, 6)
+print("Result 4:", result4)
+
+from math_operations import *
+
+result5 = add_numbers(20, 4)
+print("Result 5:", result5)
