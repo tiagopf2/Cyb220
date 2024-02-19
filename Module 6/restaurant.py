967546b267b4d7f4cd15d0dc941b93520401eb1f
tiagopf2 <156838810+tiagopf2@users.noreply.github.com>
2024-02-19 11:41:10 -0500

    Create restaurant.py

---
+++ Module 6/restaurant.py
@@ -0,0 +1,15 @@
+# Written by Tiago Perez
+
+# 9.1 Restaurant
+
+class Restaurant:
+    def __init__(self, restaurant_name, cuisine_type):
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+
+    def describe_restaurant(self):
+        print(f"Restaurant Name: {self.restaurant_name}")
+        print(f"Cuisine Type: {self.cuisine_type}")
+
+    def open_restaurant(self):
+        print(f"{self.restaurant_name} is now open!")
